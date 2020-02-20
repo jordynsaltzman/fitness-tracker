4ea9fab7c79271c996b1de31cd79c27405ec3ba6
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-02-20 14:46:00 -0500

    initial commit

---
+++ models/Workout.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: { type: String },
+      name: { type: String },
+      duration: { type: Number },
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: { type: Number },
+      distance: { type: Number }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Resistance", WorkoutSchema);
+
+module.exports = Workout;
